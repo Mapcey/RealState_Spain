3c7a3c51dce15d585ab6da0487fb0d8019a6a436
Hasitha <108875265+HasithaBJ@users.noreply.github.com>
2024-10-26 20:36:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
